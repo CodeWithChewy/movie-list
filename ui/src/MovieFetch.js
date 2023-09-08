42fcbad92f705c96087a19749b2a5d5659f02c2a
codewithchewy <srepp@twu.edu>
2023-09-08 14:28:09 -0700

    Level 2 complete

--- ui/src/MovieFetch.js
+++ ui/src/MovieFetch.js
@@ -2,28 +2,57 @@ import React, { useState, useEffect } from 'react';
 
 const MovieList = () => {
     const [movieList, setMovieList] = useState([]);
+    let [search, setSearch] = useState('');
+    let [results, setResults] = useState([]);
+    const [showResults, setShowResults] = useState(false);
+
     const url = "http://localhost:8080/movies";
 
     useEffect(() => {
-        // Fetch data when the component mounts
+
         fetch(url)
             .then(res => res.json())
             .then(data => setMovieList(data))
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-    }, []); // The empty dependency array means this effect runs once on component mount
+    }, []);
+
+    useEffect(() => {
+        const filteredMovies = movieList.filter(movie =>
+            movie.title.toLowerCase().includes(search.toLowerCase())
+        );
+        setResults(filteredMovies);
+        setShowResults(search !== '');
+    }, [search, movieList]);
+
+    const handleInputChange = (e) => {
+        setSearch(e.target.value)
+    };
 
     return (
         <div>
             <h2>Movie List</h2>
-            <ul>
-                {movieList.map(movie => (
-                    <li key={movie.id}>
-                        <h3>{movie.title}</h3>
-                    </li>
-                ))}
-            </ul>
+            <input
+                type="text"
+                placeholder="Search..."
+                value={search}
+                onChange={handleInputChange}
+            />
+            {showResults ? (
+                <ul>
+                    {results.length === 0 ? (
+                        <li>Movie is not available</li>
+                    ) : (
+                        results.map(movie => (
+                            <li key={movie.id}>
+                                <h3>{movie.title}</h3>
+                                <p>{movie.description}</p>
+                            </li>
+                        ))
+                    )}
+                </ul>
+            ) : null}
         </div>
     );
 }
