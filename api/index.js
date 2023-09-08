6cb32837938ab5c725fb73979e77e49655b058ce
codewithchewy <srepp@twu.edu>
2023-09-08 10:18:19 -0700

    Added knexfile, working through getting the server to run, will need to open postgres and create a database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const knex = require('knex')
+    (require('./knexfile.js')['development'])
+const knexfile = require('./knexfile.js')
 const app = express();
 const port = 8080
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello! This server is up and running.")
 })
 
+app.get('/movies', (req, res) => {
+    knex('movies')
+        .select('*')
+        .then((movies) => {
+            var movies = moves.map((movie) => movie);
+            res.json(movies)
+        })
+})
 app.listen(port, () => console.log(`Express is listening on port ${port}`));
